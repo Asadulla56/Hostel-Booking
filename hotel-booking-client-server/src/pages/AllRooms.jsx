4bd1a1bdf3456ab73aa2938ec2761f65ceca4978
Asadulla56 <mdasadullahoque294@gmail.com>
2025-10-20 16:37:19 +0600

    Room Details and All rooms Details added

--- hotel-booking-client-server/src/pages/AllRooms.jsx
+++ hotel-booking-client-server/src/pages/AllRooms.jsx
@@ -63,7 +63,7 @@ const AllRooms = () => {
             >
               <img
                 onClick={() => {
-                  navigate(`/rooms/${room.id}`);
+                  navigate(`/rooms/${room._id}`);
                   scrollTo(0, 0);
                 }}
                 src={room.images[0]}
