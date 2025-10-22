7ac4c7890a2e3449ae834e26c4d7e74258dbb9bc
Asadulla56 <mdasadullahoque294@gmail.com>
2025-10-23 01:02:07 +0600

    fix

--- hotel-booking-client-server/src/pages/hotelOwner/DashBoard.jsx
+++ hotel-booking-client-server/src/pages/hotelOwner/DashBoard.jsx
@@ -47,7 +47,7 @@ const DashBoard = () => {
       <h2 className="text-xl text-blue-900/70 font-medium mb-5">
         Recent Bookings
       </h2>
-      <div className="w-full max-w-3xl text-left border border-gray-300 rounded-lg max-h-80 overflow-y-scroll">
+      <div className="w-full max-w-3xl text-left  border border-gray-300 rounded-lg max-h-80 overflow-y-scroll">
         <table className="w-full">
           <thead className="bg-gray-50">
             <tr>
@@ -85,6 +85,7 @@ const DashBoard = () => {
           </tbody>
         </table>
       </div>
+      
     </div>
   );
 };
