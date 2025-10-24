120f8846c869675a37667243b9cec26633cc99f9
Asadulla56 <mdasadullahoque294@gmail.com>
2025-10-25 00:43:01 +0600

    Upload area for images

--- hotel-booking-client-server/src/pages/hotelOwner/AddRoom.jsx
+++ hotel-booking-client-server/src/pages/hotelOwner/AddRoom.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Title from "../../components/Title";
+import { assets } from "../../assets/assets";
 
 const AddRoom = () => {
   const [images, setImages] = useState({
@@ -27,6 +28,29 @@ const AddRoom = () => {
         title="Add Room"
         subTitle="Fill in the details carefully and accurate room details, pricing,and amenities,to enhance the user booking experience"
       />
+      {/* upload area Images */}
+      <p className="text-gray-800 mt-10">Images</p>
+      <div className="grid grid-cols-2 sm:flex gap-4 my-2 flex-wrap">
+        {Object.keys(images).map((key) => (
+          <label htmlFor={`roomImages${key}`} key={key}>
+            <img
+              className="max-h-13 cursor-pointer opacity-80"
+              src={
+                images[key]
+                  ? URL.createObjectURL(images[key])
+                  : assets.uploadArea
+              }
+              alt=""
+            />
+            <input
+              type="file"
+              accept="image/*"
+              id={`roomImages${key}`}
+              hidden onChange={e => setImages({...images,[key] : e.target.files[0]})}
+            />
+          </label>
+        ))}
+      </div>
     </form>
   );
 };
