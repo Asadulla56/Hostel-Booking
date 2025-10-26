7cb0bd320c88d6fe635890e1ca0fd9e2640c9744
Asadulla56 <mdasadullahoque294@gmail.com>
2025-10-27 02:08:44 +0600

    Database connected and make to the models

---
+++ hotel-booking-server/models/User.js
@@ -0,0 +1,19 @@
+import mongoose, { model } from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    _id: { type: String, require: true },
+    username: { type: String, require: true },
+    email: { type: String, require: true },
+    image: { type: String, require: true },
+    role: { type: String, enum: ["user" | "hotelOwner"], default: "user" },
+    recentSearchedCities: [{ type: String, require: true }],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+const User = model("User",userSchema)
+export default User;
