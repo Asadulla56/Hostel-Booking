41c9895f3a5bc03d3e8b66f25b17eee2f3685551
Asadulla56 <mdasadullahoque294@gmail.com>
2025-10-29 23:19:55 +0600

    added to the json and clerkwebhooks file

---
+++ hotel-booking-server/controllers/clerkWebhooks.js
@@ -0,0 +1,54 @@
+import User from "../models/User";
+import { Webhook } from "svix";
+
+const clerkWebhooks = async (req, res) => {
+  try {
+    const whook = new Webhook(process.env.CLERK_WEBHOOKS_KEY);
+    const headers = {
+        "svix-id":req.headers['svix-id'],
+        "svix-timestamp":req.headers["svix-timestamp"],
+        "svix-signature":req.headers["svix-signature"]
+    };
+
+
+    // verify
+    await whook.verify(JSON.stringify(req.body),headers);
+    //  Gatting Data from request body
+    const {data,type}= req.body
+
+    const userData = {
+        _id:data.id,
+        email:data.email_addresses[0].email.address,
+        username:data.first_name + ""+ data.last_name,
+        image:data.image_url,
+    }
+
+    // switch case events
+    switch (type) {
+        case "user.created":{
+            await User.create(userData);
+            break;
+        }
+        case "user.updated":{
+            await User.findByIdAndUpdate(data.id,userData);
+            break;
+        }
+        case "user.deleted":{
+            await User.findByIdAndDelete(data.id);
+            break;
+        }
+    
+        default:
+            break;
+    }
+        res.json({success:true,message:"Webhook Recieved"})
+
+  } catch (error) {
+    console.log(error.message);
+    res.json({success: false , message: error.message})
+  }
+};
+
+
+
+export default clerkWebhooks;
--- hotel-booking-server/server.js
+++ hotel-booking-server/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
 import { clerkMiddleware } from '@clerk/express'
+import clerkWebhooks from "./controllers/clerkWebhooks.js";
 
 
 connectDB()
@@ -12,6 +13,9 @@ app.use(cors());
 // middleware 
 app.use(express.json())
 app.use(clerkMiddleware())
+
+// Api to listen to clerk webhooks
+app.use("/api/clerk",clerkWebhooks)
 
 app.get("/", (req, res) => res.send("API is working"));
 const PORT = process.env.PORT || 3000;
